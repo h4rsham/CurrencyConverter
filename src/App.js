5d1559b996611cb122c55939012363abb67a879c
h4rsham <harsham.latif@outlook.com>
2023-07-20 19:28:01 +0100

    Fixed syntax issues for appropriate currency formatting

--- src/App.js
+++ src/App.js
@@ -27,26 +27,26 @@ function App() {
   }, [rates]);
 
   function format(number) {
-    return number.toFixed(4);
+    return number.toFixed(2);
   }
 
   function handleAmount1Change(amount1) {
-    setAmount2(format(amount1 * rates[currency2]) / rates[currency1]);
+    setAmount2(format((amount1 * rates[currency2]) / rates[currency1]));
     setAmount1(amount1);
   }
 
   function handleCurrency1Change(currency1) {
-    setAmount2(format(amount1 * rates[currency2]) / rates[currency1]);
+    setAmount2(format((amount1 * rates[currency2]) / rates[currency1]));
     setCurrency1(currency1);
   }
 
   function handleAmount2Change(amount2) {
-    setAmount1(format(amount2 * rates[currency1]) / rates[currency2]);
+    setAmount1(format((amount2 * rates[currency1]) / rates[currency2]));
     setAmount2(amount2);
   }
 
   function handleCurrency2Change(currency2) {
-    setAmount1(format(amount2 * rates[currency1]) / rates[currency2]);
+    setAmount1(format((amount2 * rates[currency1]) / rates[currency2]));
     setCurrency2(currency2);
   }
 
