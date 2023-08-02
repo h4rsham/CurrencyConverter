961bf7093d259d4fa1f2464dab15196791190e6c
h4rsham <107643995+h4rsham@users.noreply.github.com>
2023-08-02 18:15:09 +0100

    Changed URL endpoint

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => { // fetch the exchange rates from the API as soon as the component renders
     axios
       .get( // make a GET request to the API
-        `https://data.fixer.io/api/latest?access_key=${process.env.REACT_APP_API_KEY}`
+        `http://data.fixer.io/api/latest?access_key=${process.env.REACT_APP_API_KEY}`
       )
       .then((response) => { // when the response is received from the API call, do the following:
         setRates(response.data.rates); // set the rates state Array to the rates returned from the API
